c3b54ce6ae90668119d995c01dac8064f23cb319
makeevmr <art.makeev2016@yandex.ru>
2025-07-30 03:00:50 +0300

    fix: harpoon keymaps fixed

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,8 +9,8 @@ vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<M-5>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<M-6>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<M-5>", function() harpoon:list():select(5) end)
+vim.keymap.set("n", "<M-6>", function() harpoon:list():select(6) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<M-p>", function() harpoon:list():prev() end)
