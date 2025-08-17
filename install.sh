82b591520c4a4b0a59ff79710f7ed9e2e8b6bef4
makeevmr <art.makeev2016@yandex.ru>
2025-08-17 16:39:33 +0300

    fix: installation script fixed

--- install.sh
+++ install.sh
@@ -19,13 +19,11 @@ rm -rf "${INSTALL_DIR}.config"
 apt-get update
 
 apt-get install -y wget curl git lsb-release software-properties-common gnupg zip
-# wget https://apt.llvm.org/llvm.sh
-# chmod +x llvm.sh
-# ./llvm.sh 20
-# ln -s /usr/bin/clang-20 /usr/local/bin/clang
-# ln -s /usr/bin/clang++-20 /usr/local/bin/clang++
-# ln -s /usr/bin/clangd-20 /usr/local/bin/clangd
-# rm llvm.sh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "/root/.config/nvm/nvm.sh"
+unset PREFIX
+nvm install 22
 
 # XCLIP 
 apt-get install -y xclip
