d0bebe47655ebd069a95433c5bb3861d7fa8aef2
makeevmr <art.makeev2016@yandex.ru>
2025-11-18 14:52:09 +0300

    feat: json, tsx treesitter added

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,21 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "cpp", "make", "cmake", "markdown", "python", "typescript", "javascript" },
+    ensure_installed = {
+        "c",
+        "cpp",
+        "python",
+        "javascript",
+        "typescript",
+        "tsx",
+        "lua",
+        "make",
+        "cmake",
+        "vim",
+        "vimdoc",
+        "json",
+        "query",
+        "markdown"
+    },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
