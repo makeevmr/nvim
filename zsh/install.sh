dd97a2187ec89bfaf96603db17e1d52404b473cc
makeevmr <art.makeev2016@yandex.ru>
2026-04-25 13:38:45 +0300

    feat: add zsh config

---
+++ zsh/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run with superuser
+apt update && apt install -y zsh
+chsh -s $(which zsh)
+echo 'export ZDOTDIR="$HOME/.config/zsh"' > ~/.zshenv
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Plugins
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
