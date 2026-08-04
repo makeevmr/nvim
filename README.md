d73486f4086ce6379c3b7936ddc58e38859f63ec
makeevmr <art.makeev2016@yandex.ru>
2026-08-04 14:41:49 +0300

    feat: install script for all programs

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Configuration files
 Manage dotfiles with [stow](https://dev.to/crafts69guy/how-to-manage-your-dotfiles-like-a-pro-with-git-and-stow-3pg1)  
-`sudo apt update && sudo apt install stow -y`
 
-Clone project to home directory
+Clone project to home directory:
 ```bash
 cd ~
 git@github.com:makeevmr/.dotfiles.git
 ```
 
-## Create simlinks
-`stow -d ~ -t ~/.config .dotfiles`
+## Program installation
+Install programs with `./install <program_name_1> <program_name_2> ...`
+
+Available programs:
+- alacritty
+- nvim
+- tmux
+- zsh
+
+Example: `./install alacritty zsh`
---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo apt update
+
+if ! command -v git >/dev/null; then
+    sudo apt install -y git
+fi
+
+if ! command -v stow >/dev/null; then
+    sudo apt install stow -y
+fi
+
+cd ~
+stow -d ~ -t ~/.config .dotfiles
+
+install_alacritty() {
+    echo "Installing alacritty..."
+    sudo add-apt-repository ppa:aslatter/ppa -y
+    sudo apt install alacritty -y
+}
+
+install_nvim() {
+    echo "Installing nvim..."
+    sudo snap install nvim --classic
+}
+
+install_tmux() {
+    echo "Installing tmux..."
+    sudo apt install -y tmux
+}
+
+install_zsh() {
+    echo "Installing zsh..."
+
+    sudo apt install -y zsh
+    if [[ "$SHELL" != "$(which zsh)" ]]; then
+        chsh -s "$(which zsh)"
+    fi
+    echo 'export ZDOTDIR="$HOME/.config/zsh"' > ~/.zshenv
+
+    # Install Oh My Zsh
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    # Plugins
+    plugin_dir="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins"
+
+    # zsh-syntax-highlighting
+    if [[ ! -d "$plugin_dir/zsh-syntax-highlighting" ]]; then
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+            "$plugin_dir/zsh-syntax-highlighting"
+    fi
+
+    # zsh-autosuggestions
+    if [[ ! -d "$plugin_dir/zsh-autosuggestions" ]]; then
+        git clone https://github.com/zsh-users/zsh-autosuggestions \
+            "$plugin_dir/zsh-autosuggestions"
+    fi
+
+    # For arc support check out https://github.com/misbiheyv/arc-zsh-plugin
+
+    rm ~/.zshrc
+}
+
+declare -A installers=(
+    [alacritty]=install_alacritty
+    [nvim]=install_nvim
+    [tmux]=install_tmux
+    [zsh]=install_zsh
+)
+
+for component in "$@"; do
+    if [[ -v installers[$component] ]]; then
+        "${installers[$component]}"
+    else
+        echo "Unknown component: $component"
+        exit 1
+    fi
+done
